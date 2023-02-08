d1d3bd056be37c16852f5e0ebc2788b8528a74c2
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2023-02-08 14:04:35 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,17 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-    region = "us-east-1"
-    access_key = ""
-    secret_key = ""
-} resource "aws_instance" "myec2" {
+resource "aws_instance" "myec2" {
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
-    }
+    key_name="linux"
+}
