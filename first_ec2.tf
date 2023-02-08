41dc784c2d525595059a375203acb936f76aa9e0
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2023-02-08 14:16:54 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,4 +1,22 @@
-resource "aws_instance" "myec2" {
-    ami = "ami-0aa7d40eeae50c9a9"
-    instance_type = "t2.micro"
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
