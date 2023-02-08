0b6bc8c9f88ac4d43cd7fae2c63817d8fd38870e
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2023-02-08 12:36:50 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAY7345KKADH7RHSF5"
-    secret_key = "r4JUZ7XI+ssEsHHcDx0avc7VBERHh3b02vG0h2Qt"
+    access_key = "AKIA2Z7PS5SWQ2VPFN5F"
+    secret_key = "3BD7t10O9HJ0S+5iQA9zXeIs/to7OVjmjzQ/qik/"
 } resource "aws_instance" "myec2" {
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
